a755bb5778881cbfdf2d145e9382cf6510c8cbb3
Domenic Franjic <105493643+SiSanchopancho@users.noreply.github.com>
2026-02-12 11:57:26 +0100

    Update copyright year in SparseDFM.h

--- Internals/SparseDFM.h
+++ Internals/SparseDFM.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-3.0-or-later */
 /*
- * Copyright © 2026 Domenic Franjic
+ * Copyright Â© 2024-2026 Domenic Franjic
  *
  * This file is part of ReplicationNowcastingMacroVarsWithSDFM.
  *
