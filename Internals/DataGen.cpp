b0d3cbc087eff0457bd5de0d38c6ac36e48c3943
Domenic Franjic <105493643+SiSanchopancho@users.noreply.github.com>
2026-02-12 11:53:28 +0100

    Update copyright and license year in DataGen.cpp
    Updated copyright information and license year in DataGen.cpp.

--- Internals/DataGen.cpp
+++ Internals/DataGen.cpp
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-3.0-or-later */
 /*
- * Copyright © 2026 Domenic Franjic
+ * Copyright Â© 2024-2026 Domenic Franjic
  *
  * This file is part of ReplicationNowcastingMacroVarsWithSDFM.
  *
@@ -326,3 +326,4 @@ void DataGen::staticFM(
 
     return;
 }
+
